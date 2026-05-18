1dee0ea0dd875d1530000f40db9637f4f7245163
Jordan Seider <seider7@gmail.com>
2026-05-18 12:59:19 -0700

    Updated code and fixed errors

--- Scripts/ArcticDEM_IvvavikNP_Processing.R
+++ Scripts/ArcticDEM_IvvavikNP_Processing.R
@@ -5,12 +5,16 @@ library(tidyterra)
 
 setwd("C:/Users/jseider.stu")
 
-dem <- rast("./Documents/_R_Working/202505_ArcticDEM_mosiac_2m.tif")
+dem <- rast("./Sync/Data/ArcticDEM/202505_ArcticDEM_mosiac_2m.tif")
 
-ivvavik_demproj <- vect("./Documents/_R_Working/IvvavikNationalPark.shp") %>%
+ivvavik_demproj <- vect(
+  "./Sync/Data/ParksCanada/IvvavikNationalPark/IvvavikNationalPark.shp"
+) %>%
   project(dem)
 
-ivvavik <- vect("./Documents/_R_Working/IvvavikNationalPark.shp") %>%
+ivvavik <- vect(
+  "./Sync/Data/ParksCanada/IvvavikNationalPark/IvvavikNationalPark.shp"
+) %>%
   project("EPSG:32607") # WSG 84/UTM 7N
 
 # 2-metre raster
@@ -31,9 +35,10 @@ dem <- dem %>%
   )
 
 dem$aspect_ns_2m <- cos((dem$aspect_2m * pi) / 180)
-gc()
 dem$aspect_ew_2m <- sin((dem$aspect_2m * pi) / 180)
 gc()
+
+writeRaster(dem, "./Documents/_R_Working/ArcticDEM_IvvavikNP_2m.tif")
 
 # 10-metre raster
 dem_10m <- rast("./Sync/Data/ArcticDEM/202505_ArcticDEM_mosiac_10m.tif")[[
@@ -41,20 +46,26 @@ dem_10m <- rast("./Sync/Data/ArcticDEM/202505_ArcticDEM_mosiac_10m.tif")[[
 ]] %>%
   project(ivvavik) %>%
   crop(ivvavik, mask = TRUE) %>%
-  terrain(dem_10m, c("slope", "aspect", "TPI"), unit = "degrees") %>%
+  rename(dem_10m = dem)
+
+terrain_10m <- terrain(
+  dem_10m,
+  c("slope", "aspect", "TPI"),
+  unit = "degrees"
+) %>%
   rename(
-    dem_10m = dem,
     slope_10m = slope,
     aspect_10m = aspect,
     TPI_10m = TPI
-  ) %>%
-  resample(dem, method = "near")
+  )
 
-dem <- c(dem, dem_10m)
-rm(dem_10m)
+dem_10m <- c(dem_10m, terrain_10m) %>%
+  resample(dem, method = "near")
+rm(terrain_10m)
 
 # Convert aspect to northness
-dem$aspect_ns_10m <- cos((dem$aspect_10m * pi) / 180)
-dem$aspect_ew_10m <- sin((dem$aspect_10m * pi) / 180)
+dem_10m$aspect_ns_10m <- cos((dem_10m$aspect_10m * pi) / 180)
+dem_10m$aspect_ew_10m <- sin((dem_10m$aspect_10m * pi) / 180)
+gc()
 
-writeRaster(dem, "./Documents/_R_Working/Ivvavik_DEM_UTM7N.tif")
+writeRaster(dem_10, "./Documents/_R_Working/ArcticDEM_IvvavikNP_10m.tif")
