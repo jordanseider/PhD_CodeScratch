a9197c9a6fe4b47f1fa7bf891c15f3e3175f45e2
Jordan Seider <seider7@gmail.com>
2026-05-18 20:03:23 -0700

    fixed issue where 10m output was split into 2m resolution. Now, `dem_10m` is 10m resolution. Also added some code at the end to explore using GRASS for some additional topographic variables.

--- Scripts/ArcticDEM_IvvavikNP_Processing.R
+++ Scripts/ArcticDEM_IvvavikNP_Processing.R
@@ -2,6 +2,9 @@ library(terra)
 terraOptions(memfrac = 0.8)
 library(tidyverse)
 library(tidyterra)
+# BELOW: For calculating TWI with GRASS - also good for large raster processing
+library(fasterRaster)
+faster(grassDir = "C:/Program Files/GRASS GIS 8.4")
 
 setwd("C:/Users/jseider.stu")
 
@@ -59,13 +62,40 @@ terrain_10m <- terrain(
     TPI_10m = TPI
   )
 
+# Create a blank template using the 2m raster's exact spatial extent and origin
+template_10m <- rast(dem)
+# Override the template's resolution to 10 meters
+res(template_10m) <- 10
+# Stack 10m layers and resample them into the aligned 10m template
 dem_10m <- c(dem_10m, terrain_10m) %>%
-  resample(dem, method = "near")
-rm(terrain_10m)
+  resample(
+    template_10m,
+    method = "bilinear",
+    threads = TRUE
+  )
+
+rm(terrain_10m, template_10m)
 
 # Convert aspect to northness
 dem_10m$aspect_ns_10m <- cos((dem_10m$aspect_10m * pi) / 180)
 dem_10m$aspect_ew_10m <- sin((dem_10m$aspect_10m * pi) / 180)
 gc()
 
-writeRaster(dem_10, "./Documents/_R_Working/ArcticDEM_IvvavikNP_10m.tif")
+writeRaster(
+  dem_10m,
+  "./Sync/Data/ArcticDEM/IvvavikNP/ArcticDEM_IvvavikNP_10m.tif"
+)
+
+# Calculate Topographic Wetness Index with GRASS
+fastdem <- fast(
+  "./Sync/Data/ArcticDEM/IvvavikNP/ArcticDEM_IvvavikNP_10m.tif"
+)$dem_10m
+fastdem <- fast(dem_10m$dem_10m)
+
+twi_10m <- wetness(fastdem)
+
+morph <- geomorphons(
+  fastdem,
+  inner = 2,
+  outer = 20
+)
